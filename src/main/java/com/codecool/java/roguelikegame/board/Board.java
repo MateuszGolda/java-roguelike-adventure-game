cb23ccde67d6c6da55b185f3b129b24757631b61
MateuszGolda <mateusz.golda1993@gmail.com>
2020-03-24 17:51:41 +0100

    implement importing board from file, printing it, printing on it

---
+++ src/main/java/com/codecool/java/roguelikegame/board/Board.java
@@ -0,0 +1,60 @@
+package com.codecool.java.roguelikegame.board;
+
+import java.util.Scanner;
+
+import com.codecool.java.roguelikegame.UI;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+/**
+ * Board
+ */
+public class Board {
+    private String[][] board;
+
+    public Board(String fileName) {
+        try {
+            getBoardFromFile(fileName);
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+    }
+
+    public void getBoardFromFile(String fileName) throws FileNotFoundException {
+
+        String[][] board = new String[40][150];
+        Scanner scan = new Scanner(new File(fileName));
+        for (int i = 0; i < board.length; i++) {
+            board[i] = scan.nextLine().split("");
+        }
+        this.board = board;
+    }
+
+    public void printBoard() {
+        UI.clearScreen();
+        for (String[] row : board) {
+            for (String square : row) {
+                System.out.print(square);
+            }
+            System.out.println();
+        }
+    }
+
+    public void printOnBoard(String[][] item, int y, int x) {
+        for (int i = 0; i < item.length; i++) {
+            for (int j = 0; j < item[i].length; j++) {
+                UI.moveCursor(y + i, x + j);
+                System.out.print(item[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * @return the board
+     */
+    public String[][] getBoard() {
+        return board;
+    }
+}
