d9f047719631ab1b7c55a012b7c1a6ca9f6fb29a
MateuszGolda <mateusz.golda1993@gmail.com>
2020-03-25 20:42:21 +0100

    implement addEnemy and addItem methods in Stage1

--- src/main/java/com/codecool/java/roguelikegame/Stage1.java
+++ src/main/java/com/codecool/java/roguelikegame/Stage1.java
@@ -1,6 +1,9 @@
 package com.codecool.java.roguelikegame;
 
 import com.codecool.java.roguelikegame.beings.Being;
+import com.codecool.java.roguelikegame.beings.Item;
+import com.codecool.java.roguelikegame.beings.ItemTypes;
+import com.codecool.java.roguelikegame.beings.Monster;
 import com.codecool.java.roguelikegame.board.Board;
 
 /**
@@ -15,5 +18,15 @@ public class Stage1 extends Stage {
     @Override
     protected void setBoard() {
         super.board = new Board("src/main/java/com/codecool/java/roguelikegame/board/boards/map1.txt");
+    }
+
+    @Override
+    protected void addEnemies() {
+        enemies.add(new Monster(2, 2, 20, 2, 7, 50));
+    }
+
+    @Override
+    protected void addItems() {
+        items.add(new Item(ItemTypes.HP));
     }
 }
