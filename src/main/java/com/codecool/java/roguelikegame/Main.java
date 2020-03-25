a59075c522a3778b1c530f3a4da3ff5e4b8e4513
MateuszGolda <mateusz.golda1993@gmail.com>
2020-03-25 15:56:15 +0100

    create player object in the Main class

--- src/main/java/com/codecool/java/roguelikegame/Main.java
+++ src/main/java/com/codecool/java/roguelikegame/Main.java
@@ -2,13 +2,17 @@ package com.codecool.java.roguelikegame;
 
 import java.io.IOException;
 
+import com.codecool.java.roguelikegame.beings.Being;
+import com.codecool.java.roguelikegame.beings.Player;
+
 /**
  * Main
  */
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Stage stage1 = new Stage1();
+        Being player = new Player(5, 5); // set player starting position
+        Stage stage1 = new Stage1(player);
         stage1.gameLoop();
     }
 }
