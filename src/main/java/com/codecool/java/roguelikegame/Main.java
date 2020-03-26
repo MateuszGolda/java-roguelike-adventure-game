fcb196929e190a402a4807f6efaf2f75b7c8a552
MateuszGolda <mateusz.golda1993@gmail.com>
2020-03-26 16:33:59 +0100

    imlement startGame method in Main class

--- src/main/java/com/codecool/java/roguelikegame/Main.java
+++ src/main/java/com/codecool/java/roguelikegame/Main.java
@@ -1,9 +1,11 @@
 package com.codecool.java.roguelikegame;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import com.codecool.java.roguelikegame.beings.Being;
 import com.codecool.java.roguelikegame.beings.Player;
+import com.codecool.java.roguelikegame.board.Inventory;
 import com.codecool.java.roguelikegame.board.StartScreen;
 
 /**
@@ -11,13 +13,13 @@ import com.codecool.java.roguelikegame.board.StartScreen;
  */
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         boolean isRunning = true;
         while (isRunning) {
             StartScreen start = new StartScreen();
             switch (start.displayStartScreen()) {
                 case 0: // start game
-                    // code of game
+                    startGame();
                     break;
                 case 3: // quit
                     isRunning = false;
@@ -25,10 +27,12 @@ public class Main {
             }
         }
 
-        /*
+    }
+
+    public static void startGame() throws FileNotFoundException {
+        Inventory inventory = new Inventory();
         Being player = new Player(5, 5); // set player starting position
-        Stage stage1 = new Stage1(player);
+        Stage stage1 = new Stage1(player, inventory);
         stage1.gameLoop();
-        */
     }
 }
