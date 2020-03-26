40e078c5babd4c5dbd7e78e81eda750548f89ce0
Grelory <gmpwm99@gmail.com>
2020-03-26 02:21:26 +0100

    restore main in main class

--- src/main/java/com/codecool/java/roguelikegame/Main.java
+++ src/main/java/com/codecool/java/roguelikegame/Main.java
@@ -3,10 +3,7 @@ package com.codecool.java.roguelikegame;
 import java.io.IOException;
 
 import com.codecool.java.roguelikegame.beings.Being;
-import com.codecool.java.roguelikegame.beings.Item;
-import com.codecool.java.roguelikegame.beings.ItemTypes;
 import com.codecool.java.roguelikegame.beings.Player;
-import com.codecool.java.roguelikegame.board.Inventory;
 
 /**
  * Main
@@ -16,23 +13,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Being player = new Player(5, 5); // set player starting position
         Stage stage1 = new Stage1(player);
-        Inventory inventory = new Inventory();
-        inventory.addItem(new Item(ItemTypes.AGILITY, 0, 0));
-        inventory.addItem(new Item(ItemTypes.HP, 0, 0));
-        inventory.addItem(new Item(ItemTypes.STRENGTH, 0, 0));
-        inventory.addItem(new Item(ItemTypes.AGILITY, 0, 0));
-        inventory.addItem(new Item(ItemTypes.AGILITY, 0, 0));
-        inventory.addItem(new Item(ItemTypes.DEFENCE, 0, 0));
-        inventory.addItem(new Item(ItemTypes.STRENGTH, 0, 0));
-        inventory.inventoryScreen(player);
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        /*
         stage1.gameLoop();
-        */
     }
 }
