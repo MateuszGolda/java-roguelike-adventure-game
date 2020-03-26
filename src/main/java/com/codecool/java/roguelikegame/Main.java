2ade08d2ad6816ad5b06ec130b4c0db22d547bde
MateuszGolda <mateusz.golda1993@gmail.com>
2020-03-26 22:03:42 +0100

    create Stage objects in Main, and run based on the returned value

--- src/main/java/com/codecool/java/roguelikegame/Main.java
+++ src/main/java/com/codecool/java/roguelikegame/Main.java
@@ -7,6 +7,11 @@ import com.codecool.java.roguelikegame.beings.Being;
 import com.codecool.java.roguelikegame.beings.Player;
 import com.codecool.java.roguelikegame.board.Inventory;
 import com.codecool.java.roguelikegame.board.StartScreen;
+import com.codecool.java.roguelikegame.stages.Stage;
+import com.codecool.java.roguelikegame.stages.Stage1;
+import com.codecool.java.roguelikegame.stages.Stage2;
+import com.codecool.java.roguelikegame.stages.Stage3;
+import com.codecool.java.roguelikegame.stages.Stage4;
 
 /**
  * Main
@@ -26,13 +31,20 @@ public class Main {
                     break;
             }
         }
-
     }
 
     public static void startGame() throws FileNotFoundException {
         Inventory inventory = new Inventory();
         Being player = new Player(5, 5); // set player starting position
-        Stage stage1 = new Stage1(player, inventory);
-        stage1.gameLoop();
+
+        Stage[] stages = new Stage[4];
+        stages[0] = new Stage1(player, inventory);
+        stages[1] = new Stage2(player, inventory);
+        stages[2] = new Stage3(player, inventory);
+        stages[3] = new Stage4(player, inventory);
+        int currentStage = 0;
+        while (currentStage < 4) {
+            currentStage += stages[currentStage].gameLoop();
+        }
     }
 }
