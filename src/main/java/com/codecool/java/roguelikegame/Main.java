58937a9511ff2d1be1c83b60f749c6eb856cc5f2
Grelory <gmpwm99@gmail.com>
2020-03-26 14:42:03 +0100

    add StartScreen class

--- src/main/java/com/codecool/java/roguelikegame/Main.java
+++ src/main/java/com/codecool/java/roguelikegame/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import com.codecool.java.roguelikegame.beings.Being;
 import com.codecool.java.roguelikegame.beings.Player;
+import com.codecool.java.roguelikegame.board.StartScreen;
 
 /**
  * Main
@@ -11,8 +12,23 @@ import com.codecool.java.roguelikegame.beings.Player;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        boolean isRunning = true;
+        while (isRunning) {
+            StartScreen start = new StartScreen();
+            switch (start.displayStartScreen()) {
+                case 0: // start game
+                    // code of game
+                    break;
+                case 3: // quit
+                    isRunning = false;
+                    break;
+            }
+        }
+
+        /*
         Being player = new Player(5, 5); // set player starting position
         Stage stage1 = new Stage1(player);
         stage1.gameLoop();
+        */
     }
 }
---
+++ src/main/java/com/codecool/java/roguelikegame/board/StartScreen.java
@@ -0,0 +1,116 @@
+package com.codecool.java.roguelikegame.board;
+
+import com.codecool.java.roguelikegame.UI;
+import com.codecool.java.roguelikegame.beings.IconColor;
+import com.codecool.java.roguelikegame.beings.Item;
+import com.codecool.java.roguelikegame.beings.ItemTypes;
+import java.util.Scanner;
+
+public class StartScreen {
+
+    Scanner userInput = new Scanner(System.in);
+    Board board;
+    int makerIndex;
+    final int OPTIONS_HIGHT = 15;
+    final int OPTIONS_SHIFT = 67;
+    final int ACTIONS_HIGHT = 29;
+    final int ACTIONS_SHIFT = 4;
+
+    public StartScreen() {
+        makerIndex = 0;
+    }
+
+    public int displayStartScreen() {
+        while (true) {    
+            board = new Board("src/main/java/com/codecool/java/roguelikegame/board/boards/start.txt");
+            board.printBoard();
+            printPotions();
+            printOptions();
+            printActions();
+            printArrow();
+            UI.moveCursor(40, 0);
+            switch (userInput.next()) {
+                case "w":
+                    if (makerIndex > 0) {
+                        makerIndex--;
+                    }
+                    break;
+                case "s":
+                    if (makerIndex < 3) {
+                        makerIndex++;
+                    }
+                    break;
+                case "c":
+                    if (makerIndex == 1) {
+                        printBoardFrom("src/main/java/com/codecool/java/roguelikegame/board/boards/instruction.txt");
+                    } else if (makerIndex == 2) {
+                        printBoardFrom("src/main/java/com/codecool/java/roguelikegame/board/boards/credits.txt");
+                    } else {
+                        return makerIndex;
+                    }
+                    userInput.next();
+                    break;
+            }
+        }
+    }
+
+    private void printPotions() {
+        Item first = new Item(ItemTypes.AGILITY, 2, 4);
+        Item second = new Item(ItemTypes.AGILITY, 2, 139);
+        printPotionsOnBoard(first, second);
+        first = new Item(ItemTypes.STRENGTH, 2, 19);
+        second = new Item(ItemTypes.STRENGTH, 2, 124);
+        printPotionsOnBoard(first, second);
+        first = new Item(ItemTypes.DEFENCE, 2, 34);
+        second = new Item(ItemTypes.DEFENCE, 2, 109);
+        printPotionsOnBoard(first, second);
+        first = new Item(ItemTypes.HP, 2, 49);
+        second = new Item(ItemTypes.HP, 2, 94);
+        printPotionsOnBoard(first, second);
+        first = new Item(ItemTypes.AGILITY, 2, 64);
+        second = new Item(ItemTypes.AGILITY, 2, 79);
+        printPotionsOnBoard(first, second);
+
+    }
+
+    private void printPotionsOnBoard(Item first, Item second) {
+        board.printOnBoard(first.getIcon(), first.getyPosition(), first.getxPosition());
+        board.printOnBoard(second.getIcon(), second.getyPosition(), second.getxPosition());
+    }
+
+    private void printOptions() {
+        String[][] options = new String[10][1];
+        for (int i = 0; i < options.length; i++) {
+            options[i][0] = "|";
+        }
+        options[0][0] = "|START GAME";
+        options[3][0] = "|INSTRUCTION";
+        options[6][0] = "|CREDITS";
+        options[9][0] = "|QUIT";
+        board.printOnBoard(options, OPTIONS_HIGHT, OPTIONS_SHIFT);
+    }
+
+    private void printActions() {
+        String[][] actions = new String[11][1];
+        for (int i = 0; i < actions.length; i++) {
+            actions[i][0] = " ";
+        }
+        actions[0][0] = "Actions:";
+        actions[2][0] = "c - chose potion";
+        actions[4][0] = "w - move up";
+        actions[6][0] = "s - move down";
+        actions[8][0] = "Enter what you want to do and press \"ENTER\"";
+        board.printOnBoard(actions, ACTIONS_HIGHT, ACTIONS_SHIFT);
+    }
+
+    private void printArrow() {
+        String[][] arrow = {{IconColor.RED.iconColor(), ">>>>>", IconColor.RESET.iconColor()}};
+        board.printOnBoard(arrow, OPTIONS_HIGHT + 3 * makerIndex, OPTIONS_SHIFT - 7);
+    }
+
+    private void printBoardFrom(String fileName) {
+        Board boardToPrint = new Board(fileName);
+        boardToPrint.printBoard();
+    }
+
+}
